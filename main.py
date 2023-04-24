3a7320e335793d29fba1e7b37b25b83202b7c232
ErnBern <ernpingal@outlook.com>
2023-04-23 22:18:46 -0500

    Update main.py
    Added the bulk of the browser support. The program still needs to write the current browser to a settings file

--- main.py
+++ main.py
@@ -8,6 +8,69 @@ from selenium.webdriver.chrome.service import Service
 app = ctk.CTk()
 app.title("Advanced Typeracer Bot")
 
+driver = ''
+
+def change_browser(browser):
+    global driver
+    if browser == 'Brave':
+        from selenium import webdriver
+        from selenium.webdriver.chrome.service import Service as BraveService
+        from webdriver_manager.chrome import ChromeDriverManager
+        from webdriver_manager.core.utils import ChromeType
+
+        driver = webdriver.Chrome(service=BraveService(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()))
+
+    if browser == "Chrome":
+        from selenium import webdriver
+        from selenium.webdriver.chrome.service import Service as ChromeService
+        from webdriver_manager.chrome import ChromeDriverManager
+
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    
+    if browser == 'Chromium':
+        from selenium import webdriver
+        from selenium.webdriver.chrome.service import Service as ChromiumService
+        from webdriver_manager.chrome import ChromeDriverManager
+        from webdriver_manager.core.utils import ChromeType
+
+        driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+
+    if browser == 'Edge':
+        from selenium import webdriver
+        from selenium.webdriver.edge.service import Service as EdgeService
+        from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+        driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+
+    if browser == 'FireFox':
+        from selenium import webdriver
+        from selenium.webdriver.firefox.service import Service as FirefoxService
+        from webdriver_manager.firefox import GeckoDriverManager
+
+        driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+    if browser == "Internet Explorer":
+        from selenium import webdriver
+        from selenium.webdriver.ie.service import Service as IEService
+        from webdriver_manager.microsoft import IEDriverManager
+
+        driver = webdriver.Ie(service=IEService(IEDriverManager().install()))
+
+    if browser == 'Opera':
+        from selenium import webdriver
+        from selenium.webdriver.chrome import service
+        from webdriver_manager.opera import OperaDriverManager
+
+        webdriver_service = service.Service(OperaDriverManager().install())
+        webdriver_service.start()
+
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('w3c', True)
+
+        driver = webdriver.Remote(webdriver_service.service_url, options=options)
+
+    
+
 class First_Screen():
     WIDTH = 300
     HEIGHT = 150
@@ -27,7 +90,7 @@ class First_Screen():
 
         select_browser_combo = ctk.CTkComboBox(
             master=app,
-            values = ['Brave', 'Chrome', 'Firefox', 'Internet Explorer', 'Opera'],
+            values = ['Brave', 'Chrome', 'Chromium', 'Edge', 'Firefox', 'Internet Explorer', 'Opera'],
             font=self.FONT,
         )
         #Set to 'Chrome' because it's the most common browser
