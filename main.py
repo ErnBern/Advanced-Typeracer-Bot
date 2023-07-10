5d27eb2d254ae4a6b40b677b2cd46ac580775abc
ErnBern <ernpingal@outlook.com>
2023-07-10 10:32:43 -0500

    Finished Bot
    Completed Settings and clean up the bot

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import string
 import random
 import asyncio
 import threading
-import selenium
 import customtkinter as ctk
 from bs4 import BeautifulSoup
-from pynput.keyboard import Listener, KeyCode
+from pynput.keyboard import Listener
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -20,6 +19,9 @@ run = False
 
 accuracy = ""
 wpm = ""
+
+changing_keybind = False # used to check if a keybind is being changed
+keybind_display = None # entry box used to display the keybind
 
 #Variables used to get the accuracy and wpm values when using keybinds
 accuracy_ComboBox = None
@@ -119,10 +121,12 @@ def change_browser(browser):
 
 class Preferences(ctk.CTkToplevel):
     WIDTH = 500
-    HEIGHT = 450
+    HEIGHT = 300
     FONT= (17, 17)
     def __init__(self, *args, **kwargs):
+        global keybind_display
         super().__init__(*args, **kwargs)
+        self.title('Change Preferences')
         self.geometry(f"{self.WIDTH}x{self.HEIGHT}")
         self.minsize(self.WIDTH, self.HEIGHT)
         self.maxsize(self.WIDTH, self.HEIGHT)
@@ -140,46 +144,215 @@ class Preferences(ctk.CTkToplevel):
         )
         keybind_frame.pack()
         #Entry that displays the current keybind
-        self.keybind_display = ctk.CTkEntry(
+        keybind_display = ctk.CTkEntry(
             master=keybind_frame,
             font=self.FONT,
             width=200,
             height=30,
             justify="center"
         )
-        self.keybind_display.grid(pady=10, columnspan=5,sticky='ew')
+        keybind_display.grid(pady=10, columnspan=5,sticky='ew')
         self.grid_rowconfigure((0), weight=1) #Centres the entry window
         #Inserting the text into the entry
-        self.keybind_display.insert(0, f"SELECT KEYBIND")
-        self.keybind_display.configure(state="disabled")
+        keybind_display.insert(0, f"SELECT KEYBIND")
+        keybind_display.configure(state="disabled")
 
         self.change_start_button = ctk.CTkButton(
             master=keybind_frame,
             text="Change Start Keybind",
             font=self.FONT,
-            command=self.change_start_button,
+            command=self.change_start_keybind,
         )
-        self.change_start_button.grid(row=1, column=0, padx=10, sticky='w')
+        self.change_start_button.grid(row=1, column=0, padx=10, sticky='e')
 
         self.change_stop_button = ctk.CTkButton(
             master=keybind_frame,
             text="Change Stop Keybind",
             font=self.FONT,
-            command=self.change_stop_button,
+            command=self.change_stop_keybind,
         )
-        self.change_stop_button.grid(row=1, column=1, padx=10, sticky='e')
+        self.change_stop_button.grid(row=1, column=1, padx=10, sticky='w')
 
         self.grid_columnconfigure((0, 1), weight=1) #Creates and equal distance between the 2 buttons
 
+        self.confirm_keybind = ctk.CTkButton(
+            master=keybind_frame,
+            text="Confirm Keybind",
+            state='disabled',
+            command=self.confirm_keybind_change,
+            font=self.FONT,
+        )
+        self.grid_rowconfigure(2, weight=1)
+        self.confirm_keybind.grid(row=2, pady=(5, 0), columnspan=2, sticky='w', padx=(40, 0))
+
+        self.cancel_keybind = ctk.CTkButton(
+            master=keybind_frame,
+            text="Cancel",
+            command=self.cancel_keybind_change,
+            font=self.FONT,
+            state='disabled',
+        )
+        self.cancel_keybind.grid(row=2, column=1, pady=(5,0),columnspan=2, sticky='e', padx=(0, 40))
+
+        #Themes frame
+        themes_frame = ctk.CTkFrame(
+            master=self,
+            fg_color='transparent',
+        )
+        themes_frame.pack(pady=10)
+
+        #Radio Buttons for the themes/modes
+        self.theme_radio_var = ctk.IntVar(value=0)
+        light_mode = ctk.CTkRadioButton(
+            themes_frame,
+            font=self.FONT,
+            text="Light Mode",
+            value=1,
+            variable=self.theme_radio_var,
+            command=self.change_theme,
+            )
+        dark_mode = ctk.CTkRadioButton(
+            themes_frame,
+            font=self.FONT,
+            text="Dark Mode",
+            value=2,
+            variable=self.theme_radio_var,
+            command=self.change_theme,
+        )
+        system_mode = ctk.CTkRadioButton(
+            themes_frame,
+            font=self.FONT,
+            text="System Mode",
+            value=3,
+            variable=self.theme_radio_var,
+            command=self.change_theme,
+        )
+        themes_frame.grid_rowconfigure((1,2,3), weight=1)
+        light_mode.grid(row=0, pady=5, columnspan=2, sticky='ew')
+        dark_mode.grid(row=1, pady=5, columnspan=2, sticky='ew')
+        system_mode.grid(row=2, pady=5, columnspan=2, sticky='ew')
+
+        #Selects the current theme
+        if self.settings[3] == 'LIGHT':
+            light_mode.select()
+        if self.settings[3] == 'SYSTEM':
+            system_mode.select()
+        if self.settings[3] == 'DARK':
+            dark_mode.select()
+
+        browser_combobox = ctk.CTkComboBox(
+            master=self,
+            values = ['Brave', 'Chrome', 'Chromium', 'Edge', 'FireFox', 'Internet Explorer', 'Opera'],
+            font=self.FONT,
+            command=self.change_browser,
+        )
+        browser_combobox.pack(pady=5)
+        browser_combobox.set(self.settings[0])
+
+
     def change_start_keybind(self):
+        global changing_keybind
         self.change_start_button.configure(state="disabled")
-        
+        keybind_display.configure(state="normal")
+        keybind_display.delete(0, ctk.END) # Clears the Entry Box
+        keybind_display.insert(0, f"Keybind: {self.settings[1]}")
+        keybind_display.configure(state="disabled")
+        self.change_stop_button.configure(state="normal")
+        self.confirm_keybind.configure(state="normal")# Enables the confirm button
+        self.cancel_keybind.configure(state='normal')# Enables the cancel button
+        changing_keybind = True #Starts recording the keystrokes
 
     def change_stop_keybind(self):
-        pass
+        global changing_keybind
+        self.change_stop_button.configure(state="disabled")
+        keybind_display.configure(state="normal")
+        keybind_display.delete(0, ctk.END) # Clears the Entry Box
+        keybind_display.insert(0, f"Keybind: {self.settings[2]}")
+        keybind_display.configure(state="disabled")
+        self.change_start_button.configure(state="normal")
+        self.confirm_keybind.configure(state="normal")
+        self.cancel_keybind.configure(state='normal')
+        changing_keybind = True
+
+    def write_changes(self):
+        #Can't use a for loop to write changes because each line needs to be written differently
+        with open('settings.txt', 'w+') as f:
+            f.write("Do not modify this file!\n")
+            f.write(f'BROWSER={self.settings[0]}\n')
+            f.write(f'START={self.settings[1]}\n')
+            f.write(f"END={self.settings[2]}\n")
+            f.write(f"APPEARANCE={self.settings[3]}\n")
+
+    def confirm_keybind_change(self):
+        global changing_keybind
+
+        if self.change_start_button.cget('state') == 'disabled': #Checks if the start key is being changed by seeing if its button is disabled
+            keybind = keybind_display.get().split('Keybind: ')[1]
+            self.settings[1] = keybind
+            self.change_start_button.configure(state="normal")
+            start_button.configure(text=f"Start ({keybind})")
+
+        if self.change_stop_button.cget('state') == 'disabled': #Checks if the stop key is being change by checking if its button is disabled
+            keybind = keybind_display.get().split('Keybind: ')[1]
+            self.settings[2] = keybind
+            self.change_stop_button.configure(state="normal")
+            end_button.configure(text=f"Stop ({keybind})")
+
+        self.write_changes()
+        
+        #Reverts the keybind display entry back to normal
+        keybind_display.configure(state='normal')
+        keybind_display.delete(0, ctk.END)
+        keybind_display.insert(0, 'SELECT KEYBIND')
+        keybind_display.configure(state='disabled')
+
+        #Reverts the confirm and cancel button back to their orignal states
+        self.confirm_keybind.configure(state='disabled')
+        self.cancel_keybind.configure(state='disabled')
+        
+    
+    def change_theme(self):
+        theme_value = self.theme_radio_var.get()
+        theme = None
+        if theme_value == 1:
+            theme = "light"
+        if theme_value == 2:
+            theme = "dark"
+        if theme_value == 3:
+            theme = "system"
+
+        ctk.set_appearance_mode(theme)
+
+        self.settings[3] = theme.upper()
+        
+        self.write_changes()
+
+    def change_browser(self, browser):
+        self.settings[0] = browser
+
+        self.write_changes()
+        
+        driver.quit()
+        set_driver(browser)     
+        driver.get("https://play.typeracer.com/")
+
+    def cancel_keybind_change(self):
+        global changing_keybind
+        
+        self.change_stop_button.configure(state="normal")# Re-enables the start and stop buttons
+        self.change_start_button.configure(state="normal")
+
+        changing_keybind = False #Stops Recording keystrokes
+        self.cancel_keybind.configure(state='disabled') #Disables the Cancel and Confirm Button
+        self.confirm_keybind.configure(state='disabled')
+        
+        keybind_display.configure(state='normal')
+        keybind_display.delete(0, ctk.END)
+        keybind_display.insert(0, 'SELECT KEYBIND')
+        keybind_display.configure(state='disabled')
 
 
-            
+        
 class First_Screen():
     WIDTH = 300
     HEIGHT = 150
@@ -199,7 +372,7 @@ class First_Screen():
 
         select_browser_combo = ctk.CTkComboBox(
             master=app,
-            values = ['Brave', 'Chrome', 'Chromium', 'Edge', 'Firefox', 'Internet Explorer', 'Opera'],
+            values = ['Brave', 'Chrome', 'Chromium', 'Edge', 'FireFox', 'Internet Explorer', 'Opera'],
             font=self.FONT,
         )
         #Set to 'Chrome' because it's the most common browser
@@ -215,10 +388,9 @@ class First_Screen():
         select_browser_button.pack(pady=15)
 
 class Main_Window():
-    WIDTH = 500
-    HEIGHT = 450
+    WIDTH = 425
+    HEIGHT = 200
     FONT = (17, 17)
-    TOP_LEVEL_EXISTS = False
     def __init__(self):
         global start_button, end_button, accuracy_ComboBox, wpm_Entry
         app.geometry(f"{self.WIDTH}x{self.HEIGHT}")
@@ -236,13 +408,17 @@ class Main_Window():
         ctk.CTkLabel(
             entry_frame,
             font=self.FONT,
-            text='Select Accuracy:'
-        ).grid()
+            text='Accuracy:',
+            justify="center",
+        ).grid(sticky='w', columnspan=2)
+        entry_frame.grid_columnconfigure((0,1), weight=1)
+        entry_frame.grid_rowconfigure((0), weight=1)
         ctk.CTkLabel(
             entry_frame,
             font=self.FONT,
-            text='Select Typing Speed:'
-        ).grid(row=0, column=1)
+            text='Speed:',
+            justify='center',
+        ).grid(row=0, column=1, columnspan=2,sticky='w', padx=(20,0))
 
         #Creating the accuracy combo box
         accuracy = ctk.CTkComboBox(
@@ -256,7 +432,7 @@ class Main_Window():
             ],
             width=90
         )
-        accuracy.grid(row=1, column=0)
+        accuracy.grid(row=1, column=0, padx=(0, 40))
         accuracy_ComboBox = accuracy
 
         #Creating the typing speed entry box
@@ -264,7 +440,7 @@ class Main_Window():
             entry_frame,
             font=self.FONT,
             placeholder_text="120",
-            width=100,
+            width=90,
 
         )
         wpm.grid(row=1, column=1)
@@ -440,10 +616,19 @@ async def typer():
 
 #Function for listening for the starting and ending keys
 async def on_press(key):
+    global changing_keybind
     await asyncio.sleep(0.05)
     try: key = str(key).split('Key.')[1]
     #Stopping the char from being wrapped in ""
     except: key = str(key.char).split()[0]
+
+    if changing_keybind:
+        keybind_display.configure(state='normal')
+        keybind_display.delete(0, ctk.END)
+        keybind_display.insert(0, f"Keybind: {key.upper()}")
+        keybind_display.configure(state='disabled')
+        
+
     with open('settings.txt', 'r') as f:
         settings = f.readlines()
         #Getting the keybinds and removing the "START=" and "END=", and also the "\n"
@@ -485,6 +670,12 @@ def main():
         browser = browser.split("BROWSER=")[1]
         set_driver(browser)
         driver.get("https://play.typeracer.com/")
+
+        #sets the theme/mode from the from the settings file
+        theme = settings[4].strip()
+        theme = theme.split("APPEARANCE=")[1]
+        ctk.set_appearance_mode(theme.lower())
+
     app.mainloop()
     end_typing_thread = True
     driver.quit()
